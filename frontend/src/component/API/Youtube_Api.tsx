c47968031845da6ca98fc05f440277e2542f1898
Đào Thị Thu Hường <126840583+daohuong2004@users.noreply.github.com>
2025-05-14 11:56:46 +0700

    check api

--- frontend/src/component/API/Youtube_Api.tsx
+++ frontend/src/component/API/Youtube_Api.tsx
@@ -1,12 +1,16 @@
+ // src/component/API/Youtobe_Api.tsx
 import axios from "axios";
 
-const KEY = "AIzaSyCTXMbVAAFr8muCvl77GUjIdzRB7f8qdBE";
-
+// const KEY = "AIzaSyCTXMbVAAFr8muCvl77GUjIdzRB7f8qdBE";
+const API_KEY = "AIzaSyBOFUcW7CR8Go_Q0Yls0auFN97RwTQxPv0"; 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${query}&key=${API_KEY}&type=video",
-  params: {
-    part: "snippet",
-    maxResults: 10,
-    key: KEY,
-  },
-});
+  });
+// export default axios.create({
+//   baseURL: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${query}&key=${API_KEY}&type=video",
+//   params: {
+//     part: "snippet",
+//     maxResults: 10,
+//     key: KEY,
+//   },
+// });
